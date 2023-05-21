a65dc237d05cca1cbf83c22c510e33bcb6ed658e
Vladyslav <savelyevlad@gmail.com>
2023-05-21 17:02:17 +0200

    api key and secret

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,10 @@
 class Stock < ApplicationRecord
   
   def self.new_lookup(ticker_symbol)
-    client = Binance::Client::REST.new
+    ticker_symbol.upcase!
+    api_key = Rails.application.credentials.binance[:api_key]
+    api_secret = Rails.application.credentials.binance[:secret_key]
+    client = Binance::Client::REST.new api_key: api_key, secret_key: api_secret
     client.price(symbol: ticker_symbol)['price'].to_f
   end
 
