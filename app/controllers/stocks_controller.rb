710104d24588e2cde9a29fcad75c20b7f439abc3
Vladyslav <savelyevlad@gmail.com>
2023-05-22 15:32:12 +0200

    render symbol

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,10 +1,23 @@
 class StocksController < ApplicationController
 
   def search
-
-    stock = Stock.new_lookup(params[:stock])
-    render json: stock
-
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        if @stock.last_price == 0
+          flash[:alert] = 'No such symbol'
+          redirect_to my_portfolio_path
+        else
+          render 'users/my_portfolio'
+        end
+      else
+        flash[:alert] = 'Cannot connect to binance server'
+        redirect_to my_portfolio_path
+      end
+    else
+      flash[:alert] = 'Please enter a symbol to search'
+      redirect_to my_portfolio_path
+    end
   end
 
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,8 +4,13 @@ class Stock < ApplicationRecord
     ticker_symbol.upcase!
     api_key = Rails.application.credentials.binance[:api_key]
     api_secret = Rails.application.credentials.binance[:secret_key]
-    client = Binance::Client::REST.new api_key: api_key, secret_key: api_secret
-    client.price(symbol: ticker_symbol)['price'].to_f
+    begin
+      client = Binance::Client::REST.new api_key: api_key, secret_key: api_secret
+      price = client.price(symbol: ticker_symbol)['price'].to_f
+      new(ticker: ticker_symbol, name: ticker_symbol, last_price: price)
+    rescue => exception
+      return nil      
+    end
   end
 
 end
