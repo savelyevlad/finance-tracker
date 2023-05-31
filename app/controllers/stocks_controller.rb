279cf193d580dd552ce7995aaba41143bcbeaa10
Vladyslav <savelyevlad@gmail.com>
2023-05-31 10:33:00 +0200

    results fix

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -5,20 +5,27 @@ class StocksController < ApplicationController
       @stock = Stock.new_lookup(params[:stock])
       if @stock
         if @stock.last_price == 0
-          flash[:alert] = 'No such symbol'
-          redirect_to my_portfolio_path
+          respond_to do |format|
+            @stock = nil
+            flash.now[:alert] = 'No such symbol'
+            format.js { render partial: 'users/result_js' }
+          end
         else
           respond_to do |format|
             format.js { render partial: 'users/result_js' }
           end
         end
       else
-        flash[:alert] = 'Cannot connect to binance server'
-        redirect_to my_portfolio_path
+        respond_to do |format|
+          flash.now[:alert] = 'Cannot connect to binance server'
+          format.js { render partial: 'users/result_js' }
+        end
       end
     else
-      flash[:alert] = 'Please enter a symbol to search'
-      redirect_to my_portfolio_path
+      respond_to do |format|
+        flash.now[:alert] = 'Please enter a symbol to search'
+        format.js { render partial: 'users/result_js' }
+      end
     end
   end
 
