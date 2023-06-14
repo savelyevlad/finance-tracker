88dc720d36ea2e7d4bba87f21ea0ffc7e89c9a93
Vladyslav <savelyevlad@gmail.com>
2023-06-14 14:58:52 +0200

    friends list + removing friends

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
+  resources :friendships, only: [:destroy]
   devise_for :users
   devise_scope :user do  
-    get '/users/sign_out' => 'devise/sessions#destroy'     
+    get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
   root "welcome#index"
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'my_friends', to: 'users#my_friends'
   get 'search_stock', to: 'stocks#search'
 end
