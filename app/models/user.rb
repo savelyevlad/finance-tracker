7698bc117421ebdf59ab2600d75f819eb4253f17
Vladyslav <savelyevlad@gmail.com>
2023-06-19 15:01:15 +0200

    can track stock fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
   def stock_already_tracked?(ticker_symbol)
     stock = Stock.check_db(ticker_symbol)
     return false unless stock
-    Stock.where(id: stock.id).exists?
+    stocks.where(id: stock.id).exists?
   end
 
   def full_name
