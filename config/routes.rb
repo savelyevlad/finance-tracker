268522c90d104df3f63a406a44ff50ac7ddf0944
Vladyslav <savelyevlad@gmail.com>
2023-06-19 15:01:46 +0200

    show user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
   resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show]
   devise_for :users
   devise_scope :user do  
     get '/users/sign_out' => 'devise/sessions#destroy'
