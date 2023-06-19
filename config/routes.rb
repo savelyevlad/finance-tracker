5546620ebfeb1aa0f45bb4e3cf4901b579a1b182
Vladyslav <savelyevlad@gmail.com>
2023-06-19 15:11:23 +0200

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-  resources :user_stocks, only: [:create, :destroy]
-  resources :friendships, only: [:create, :destroy]
-  resources :users, only: [:show]
   devise_for :users
   devise_scope :user do  
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-
   root "welcome#index"
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'my_friends', to: 'users#my_friends'
   get 'search_stock', to: 'stocks#search'
   get 'search_friend', to: 'users#search'
+  resources :user_stocks, only: [:create, :destroy]
+  resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show]
 end
